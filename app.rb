1e69ab3520df08cbdd4d58eab5ca986d2553ec03
Jason Pitts <juniorjr083@gmail.com>
2018-08-07 14:39:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,19 +7,12 @@ get '/newteam' do
     erb :newteam
 end
 
-post '/team' do 
-    @name= params["name"]
-    @coach = params["coach"]
-    @sg = params["sg"]
-    @pg = params["pg"]
-    @sf  = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]
-    
-      
-     erb :team
+
+    post '/team' do
+    @team = params
+    erb :team
   
-end  
+   end  
    
    
    
